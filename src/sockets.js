0abf4cf386146b4ba09eacae56d4b77a8848d7a8
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-06-30 04:28:00 -0700

    Update sockets.js

--- src/sockets.js
+++ src/sockets.js
@@ -1,8 +1,5 @@
 import io from "socket.io-client";
 
-
-const socket = io.connect("http://localhost:3000");
-
-
+const socket = io.connect("https://ar-medicare-backend.herokuapp.com/");
 
 export default socket;
